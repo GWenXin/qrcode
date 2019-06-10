660f0af624e6969f5c27e7b89ce79db9bfe8f42a
GWenXin <35371618+GWenXin@users.noreply.github.com>
2019-06-10 17:21:37 +0800

    Update BaconQrCodeGenerator.php

--- src/BaconQrCodeGenerator.php
+++ src/BaconQrCodeGenerator.php
@@ -302,7 +302,7 @@ class BaconQrCodeGenerator implements QrCodeInterface
         $this->merge_icon = file_get_contents($merge_icon);
 
         //qrcode merge icon
-        $QR = imagecreatefromstring($qrCode);   // qr code after
+        $QR = imagecreatefromstring($imagick);   // qr code
         $logo = imagecreatefrompng($merge_icon); // icon 
         $QR_width = imagesx($QR);                // qr code width
         $QR_height = imagesy($QR);               // qr code height
