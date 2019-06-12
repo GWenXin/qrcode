f02d965470cd58fadc2531212d13fe305bee0777
GWenXin <35371618+GWenXin@users.noreply.github.com>
2019-06-12 15:09:20 +0800

    Update BaconQrCodeGenerator.php

--- src/BaconQrCodeGenerator.php
+++ src/BaconQrCodeGenerator.php
@@ -85,8 +85,8 @@ class BaconQrCodeGenerator implements QrCodeInterface
             return $qrCode;
            //return $qrCode->save(public_path('qrcode.png'));
         }
-        return file_put_contents($filename, $qrCode);
-//         return $qrCode->save(public_path('qrcode.png'));
+//        return file_put_contents($filename, $qrCode);
+        return downloadFile($filename, $qrCode);
     }
 
     /**
