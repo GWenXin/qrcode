52a79c5f9215ed21bc98090e68c47ce70062a487
GWenXin <35371618+GWenXin@users.noreply.github.com>
2019-06-12 17:27:17 +0800

    Update BaconQrCodeGenerator.php

--- src/BaconQrCodeGenerator.php
+++ src/BaconQrCodeGenerator.php
@@ -84,10 +84,9 @@ class BaconQrCodeGenerator implements QrCodeInterface
         if ($filename === null) {
             return $qrCode;
         }          
-//         return file_put_contents($filename, $qrCode);   
-//         $return Storage::disk('public')->put('qrcode.png', $qrCode);       
-        $code = file_put_contents($filename, $qrCode);
-        return $code;
+           return file_put_contents($filename, $qrCode);   
+//         return Storage::disk('public')->put('qrcode.png', $qrCode); 
+//         return $qrCode;
     }
 
     /**
