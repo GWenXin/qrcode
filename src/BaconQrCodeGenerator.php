14e5f6f6e71bd0469d81c9f820d86da5b16c3c9f
GWenXin <35371618+GWenXin@users.noreply.github.com>
2019-06-13 15:37:54 +0800

    Update BaconQrCodeGenerator.php

--- src/BaconQrCodeGenerator.php
+++ src/BaconQrCodeGenerator.php
@@ -84,10 +84,10 @@ class BaconQrCodeGenerator implements QrCodeInterface
         if ($filename === null) {
             return $qrCode;
         }          
-            $this->$curve->curve($curve_width, $curve_height);
-            $this->$merge_icon->merge_icon($merge_icon);
-            $this->$frame->frame($frame_path, $frame_width, $frame_height);
-            $this->$position->position($position_x, $position_y);
+            $this->curve($curve_width, $curve_height);
+            $this->merge_icon($merge_icon);
+            $this->frame($frame_path, $frame_width, $frame_height);
+            $this->position($position_x, $position_y);
         
 //           return file_put_contents($filename, $qrCode);   
           return $this->file_put_contents($filename, $qrCode);
